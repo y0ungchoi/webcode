dfc4087bc5499d7baf69bac97b23a008161a6c99
Eunyoung Choi <emily7485@naver.com>
2019-07-15 18:23:04 +0900

    Update MainVo.java

--- src/com/hk/th/vo/MainVo.java
+++ src/com/hk/th/vo/MainVo.java
@@ -2,9 +2,21 @@ package com.hk.th.vo;
 
 public class MainVo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String prod_nmk;
+	int prod_rating;
+	
+	public String getProd_nmk() {
+		return prod_nmk;
 	}
-
+	public void setProd_nmk(String prod_nmk) {
+		this.prod_nmk = prod_nmk;
+	}
+	public int getProd_rating() {
+		return prod_rating;
+	}
+	public void setProd_rating(int prod_rating) {
+		this.prod_rating = prod_rating;
+	}
+	
+	
 }
